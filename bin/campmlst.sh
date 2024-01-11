c518064e0ccd6383aee87579769d50c0b1a6017e
dhineshp565 <dhp565@usask.ca>
2024-01-11 16:25:43 -0600

    revised campmlst

--- bin/campmlst.sh
+++ bin/campmlst.sh
@@ -5,19 +5,14 @@
 
 # Campylbacter fetus venerealis typing
 ST="$(tail -n +2 $1 | cut -f 3)"
-if [  $ST -eq "4" -o $ST -eq "7" -o $ST -eq "12" ];then
-	echo "ORGANISM" > temp.csv
-	echo "Campylobacter fetus. venerealis" >> temp.csv
-	paste $1 temp.csv > $(basename $1 .csv)_results.csv
+if [[  $ST =~ ^(4|7|12) ]];then
+	subspecies="Campylobacter fetus. venerealis" 
 # Campylbacter fetus fetus typing	
-elif [ $ST -eq "1" -o $ST -eq "2" -o $ST -eq "3" -o $ST -eq "5" -o $ST -eq "6" -o $ST -eq "8" -o $ST -eq "9" -o $ST -eq "10" -o $ST -eq "11" -o $ST -eq "13" -o $ST -eq "14" ];then
-	echo "ORGANISM" > temp.csv
-	echo "Campylobacter fetus. fetus" >> temp.csv
-	paste $1 temp.csv > $(basename $1 .csv)_results.csv
+elif [[ $ST =~ ^(1|2|3|5|6|8|9|10|11|13|14)$ ]];then
+	subspecies="Campylobacter fetus. fetus"
 # handle No ST typing		
 else 
-	echo "ORGANISM" > temp.csv
-	echo "NA" >> temp.csv
-	paste $1 temp.csv > $(basename $1 .csv)_results.csv
-		
+	subspecies="NA"		
 fi
+# add results
+paste "$1" <(printf "ORGANISM\n%s" "$subspecies") > $(basename $1 .csv)_results.csv
