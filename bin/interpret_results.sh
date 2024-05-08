1be172b3a6a682dffb04d0f6746b048486689c76
Dhinesh Periyasamy <64551109+dhineshp565@users.noreply.github.com>
2024-05-08 12:42:17 -0600

    replaced _ with space

--- bin/interpret_results.sh
+++ bin/interpret_results.sh
@@ -6,7 +6,7 @@
 # $1 = SampleName, $2 = List of target names
 
 # start by creating the header of the csv file
-echo -e "SAMPLEID,PATHOGEN,RESULT,STATUS" > "$1_results.csv"
+echo -e "SAMPLEID,PATHOGEN,RESULT,QC STATUS" > "$1_results.csv"
 
 # loop through the list of target names
 while read lines; do
@@ -22,4 +22,6 @@ done < $2
 # check for the presence of Internal_amplification_control,Negative in the csv file
 if grep -Fq "Internal_amplification_control,Negative" "$1_results.csv"; then
     sed -i 's/OK/Check for PCR inhibition/g' "$1_results.csv" # if the Internal_amplification_control is negative then change status
+    
 fi
+sed -i 's/_/ /g' "$1_results.csv"
